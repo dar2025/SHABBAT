48591ff286ee6fbe266a4e587c1e6e1efa096468
dar2025 <uzidar1@gmail.com>
2025-11-22 21:01:40 +0200

    Create index.js

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+// Placeholder for the missing index.js file
+
+/**
+ * פונקציית Placeholder שנוצרה כדי לעבור את שלב ה-Build.
+ */
+export function createPageUrl(pageName) {
+    // מניחים נתיב פשוט עבור ה-Build
+    return pageName.toLowerCase();
+}
