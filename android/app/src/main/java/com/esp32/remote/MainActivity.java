f0d0d2127cffbfd050db972f2fdbf2ee37dd8819
[dar2025!] <[uzidar1@gmail.com)>
2025-12-02 19:58:40 +0200

    first commit

--- android/app/src/main/java/com/esp32/remote/MainActivity.java
+++ android/app/src/main/java/com/esp32/remote/MainActivity.java
@@ -1,5 +1,16 @@
 package com.esp32.remote;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // מאפשר פתיחת DevTools מהדפדפן במחשב
+        WebView.setWebContentsDebuggingEnabled(true);
+    }
+}
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path' 
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- הגדרה זו קריטית למסך הלבן ב-Capacitor, מאלצת נתיב יחסי.
+  base: './',  // חובה עבור Capacitor
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), 
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+    outDir: 'dist',    // תיקייה של Capacitor
+    emptyOutDir: true, // מנקה לפני build
   },
 })
